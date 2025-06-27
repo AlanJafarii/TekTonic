30bf13107b2d95faa9efa4013282bfd9b1d9235c
Alan <alanjafariofficial@gmail.com>
2025-06-27 02:32:25 -0700

    Update MainActivity.kt

--- android/app/src/main/kotlin/retrytech/chatter/untitled/MainActivity.kt
+++ android/app/src/main/kotlin/retrytech/chatter/untitled/MainActivity.kt
@@ -1,15 +1,5 @@
 package com.retrytech.chatter.untitled
 
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant // این خط را اضافه می کنیم
 
-class MainActivity : FlutterActivity() {
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        // این خط بسیار مهم است:
-        // GeneratedPluginRegistrant.registerWith(flutterEngine)
-        // تمام پلاگین ها را به صورت خودکار رجیستر می کند.
-        // نباید پلاگین ها را دستی اضافه کرد.
-        GeneratedPluginRegistrant.registerWith(flutterEngine) // این خط را اضافه یا مطمئن می شویم که وجود دارد
-    }
-}
+class MainActivity : FlutterActivity()
